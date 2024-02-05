7201c8cbf10e8db84545933dcb42373040bd6b05
Woland-prj <nzimin93@gmail.com>
2024-02-05 17:47:30 +0300

    some client and server configurations

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { config } from 'dotenv'
+import { VersioningType } from '@nestjs/common'
 
 async function bootstrap() {
   const envConfRes = config({ path: '../.env' })
   if (envConfRes.error) throw envConfRes.error
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('/api')
+  app.enableVersioning({
+    type: VersioningType.URI
+  })
   await app.listen(process.env.SERVER_PORT)
 }
 bootstrap()

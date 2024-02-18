0d65afa462dd7b88225f09bf8ee96b997672e05b
Woland-prj <nzimin93@gmail.com>
2024-02-19 00:49:11 +0300

    mail server dev

--- server/src/auth/mail.service.ts
+++ server/src/auth/mail.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable } from '@nestjs/common'
+import * as nodemailer from 'nodemailer'
 
 @Injectable()
 export class MailService {
-  async validateMail() {
-    console.log('validate mail')
-  }
+	private transporter
+	constructor() {
+		this.transporter = nodemailer.createTransport({
+			host: process.env.SMTP_HOST,
+			port: process.env.SMTP_PORT,
+			secure: false,
+			auth: {
+				user: process.env.SMTP_USER,
+				pass: process.env.SMTP_PASSWORD
+			}
+		})
+	}
+	async sendActivationMail(to: string, link: string) {
+		await this.transporter.sendMail({
+			from: process.env.SMTP_USER,
+			to,
+			subject: 'Активация аккаунта в таксаменеджере',
+			text: '',
+			html: `
+              <div>
+                <h1>Перейдите по ссылке для активации аккаунта</h1>
+                <a href="${link}">Активировать</a>
+              </div>
+            `
+		})
+	}
 }

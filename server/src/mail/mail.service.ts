8d354f31ac80bfcdff437129a1a88da1ae3dffe2
Woland-prj <nzimin93@gmail.com>
2024-02-20 00:54:35 +0300

    mail server dev

--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -4,23 +4,19 @@ import { Transporter, createTransport } from 'nodemailer'
 @Injectable()
 export class MailService {
 	private transporter: Transporter
-	// constructor() {
-	// 	this.transporter = createTransport({
-	// 		host: process.env.SMTP_HOST,
-	// 		port: +process.env.SMTP_PORT,
-	// 		secure: false,
-	// 		auth: {
-	// 			type: 'OAuth2',
-	// 			user: process.env.SMTP_USER,
-	// 			clientId: '440888034054-aneuit9t8fidrps8aqkmp326tmra775u.apps.googleusercontent.com',
-	// 			clientSecret: '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',
-	// 			refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
-	// 			provisionCallbackaccessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x',
-	// 			expires: 1484314697598
-	// 		}
-	// 	})
-	// }
+	constructor() {
+		this.transporter = createTransport({
+			host: process.env.SMTP_HOST,
+			port: +process.env.SMTP_PORT,
+			secure: true,
+			auth: {
+				user: process.env.SMTP_USER,
+				pass: process.env.SMTP_PASSWORD
+			}
+		})
+	}
 	async sendActivationMail(to: string, link: string) {
+		// TODO: обработка ошибок не существующего email
 		await this.transporter.sendMail({
 			from: process.env.SMTP_USER,
 			to: to,

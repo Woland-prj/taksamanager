20a462635f19e5a4e311416902ba8f0d0d5f4058
Woland-prj <nzimin93@gmail.com>
2024-03-03 00:38:47 +0300

    попытка в бота

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,14 +4,16 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { AuthService } from './auth/auth.service'
-import { PrismaModule } from './prisma/prisma.module'
-import { UsersModule } from './users/users.module'
 import { MailModule } from './mail/mail.module'
 import { MailService } from './mail/mail.service'
+import { PrismaModule } from './prisma/prisma.module'
+import { TgbotModule } from './tgbot/tgbot.module'
+import { BotService } from './tgbot/tgbot.service'
+import { UsersModule } from './users/users.module'
 
 @Module({
-	imports: [UsersModule, PrismaModule, AuthModule, MailModule],
-	providers: [AppService, AuthService, JwtService, MailService],
+	imports: [UsersModule, PrismaModule, AuthModule, MailModule, TgbotModule],
+	providers: [AppService, AuthService, JwtService, MailService, BotService],
 	controllers: [AppController]
 })
 export class AppModule {}

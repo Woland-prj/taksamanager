9f307a8dfd497980922f1776f4f33ee90df02a90
Woland-prj <nzimin93@gmail.com>
2024-04-10 23:46:07 +0300

    fix отсутствия типа у задачи

--- server/src/tasks/entities/task.entity.ts
+++ server/src/tasks/entities/task.entity.ts
@@ -10,11 +10,21 @@ export class TaskQ {
 export enum DefaultFields {
 	NAME = 'Название задачи',
 	DEADLINE = 'Дедлайн сдачи работы',
-	CLIENTNAME = 'Имя заказчика'
+	CLIENTNAME = 'Имя заказчика',
+	TYPE = 'Вид задачи'
+}
+
+export enum TaskFormType {
+	FORM_PHOTO = 'Фотосъемка',
+	FORM_VIDEO = 'Видеосъемка / анимация',
+	FORM_POST = 'Текст поста',
+	FORM_DESIGN = 'Дизайн',
+	FORM_MONTAGE = 'Монтаж'
 }
 
 export type DefaultTemplates = {
 	nameTemp: QuestionTemplate
 	deadlineTemp: QuestionTemplate
 	clientTemp: QuestionTemplate
+	typeTemp: QuestionTemplate
 }

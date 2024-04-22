cb7fcf3a769a2987f47ca9cd8979b4bf535263cf
Woland-prj <nzimin93@gmail.com>
2024-04-22 23:14:51 +0300

    dome docker and compose changes, adding proxy and client support, full app support

--- scripts/proxy.mjs
+++ scripts/proxy.mjs
@@ -5,12 +5,12 @@ const FRONTEND_URL = 'http://localhost:3100'
 const BACKEND_URL = 'http://localhost:3200'
 
 const pathMapper = {
-	'/api/v1/?(.*)': BACKEND_URL,
-	'/?(.*)': FRONTEND_URL
+  '/api/v1/?(.*)': BACKEND_URL,
+  '/?(.*)': FRONTEND_URL
 }
 
 new ReverseProxyPathMapper({}, pathMapper).serve(PROXY_PORT)
 
 setTimeout(() => {
-	console.log(`[ PROXY ] on http://localhost:${PROXY_PORT}`)
+  console.log(`[ PROXY ] on http://localhost:${PROXY_PORT}`)
 }, 7000)

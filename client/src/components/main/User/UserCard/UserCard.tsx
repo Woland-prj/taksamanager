8718137623563a5388a706a29394cb0d0a19444b
unknown <denkist555@gmail.com>
2024-05-07 08:14:34 +0300

    Сохранение перед переустановкой проекта

---
+++ client/src/components/main/User/UserCard/UserCard.tsx
@@ -0,0 +1,13 @@
+export const UserCard = () => {
+    return (
+        <div>
+            <div>
+                {/* <Image/> */}
+                <input type='file' accept='.jpg .png'></input>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
